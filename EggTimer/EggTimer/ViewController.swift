353efc2f4eaf7d98ff389f19ab4cd260b8209512
Armando Lima <armandomattei1997@gmail.com>
2020-11-07 20:03:01 -0300

    Creating the logic to set the countdown time based on the button that the user has selected

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
         let hardness = sender.currentTitle!
         
-        print(eggTimes[hardness]!)
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateCounter() {
+        
+        if secondsRemaining > 0 {
+            print(secondsRemaining)
+            secondsRemaining -= 1
+        }
     }
     
 
