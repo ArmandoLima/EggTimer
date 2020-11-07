9c49ab9bfe052ed5daf9a361f05b4e8339895174
Armando Lima <armandomattei1997@gmail.com>
2020-11-07 19:47:39 -0300

    Deleting variables

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,10 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
-    
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
