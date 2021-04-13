ea0a7e8b2a4594391524d8ff854b64bb21e0eb83
ArmandoLima <42722229+ArmandoLima@users.noreply.github.com>
2021-04-13 08:27:28 -0300

    Updating EggTimer with Final Version

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,24 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    //OUTLETS
     
-    var secondsRemaining = 60
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
+    //VARIABLES
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    //ACTIONS & FUNCS
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
+        timer.invalidate()
         let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        secondsRemaining = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        lblTitle.text = hardness
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
 
     @objc func updateCounter() {
         
-        if secondsRemaining > 0 {
-            print(secondsRemaining)
-            secondsRemaining -= 1
+        if secondsPassed < totalTime{
+            
+            secondsPassed += 1
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        }else{
+            timer.invalidate()
+            lblTitle.text = "Done!"
         }
     }
     
